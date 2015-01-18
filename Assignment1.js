d2822bad6b761b7fa3bba5dcd0c5078c7f1a1011
Stephen Burgos <stephen.burgos782004@gmail.com>
2015-01-18 09:55:22 -0500

    Added previous files

---
+++ Assignment1.js
@@ -0,0 +1,23 @@
+// Stephen Burgos from textbook for CIS 363
+// Last Changed: Jan.7.15
+
+var censoredWords = ["damn", "hell", "darn", "heck"];
+var customCensoredWords = [];
+function censor(inStr) {
+	for (idx in censoredWords) {
+		inStr = inStr.replace(censoredWords[idx], "*pickle*");
+	}
+	for (idx in customCensoredWords) {
+		inStr = inStr.replace(customCensoredWords[idx], "*pickle*");
+	}
+	return inStr;
+}
+function addCensoredWord(word){
+	customCensoredWords.push(word);
+	}
+function getCensoredWords(){
+	return censoredWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
